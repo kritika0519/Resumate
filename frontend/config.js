f78c3efd64852940f0bcac3d7e67452821c086a3
Kritika Aggarwal <kritikaaggarwal19@gmail.com>
2026-05-24 18:49:32 +0530

    Update Render backend URL

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 window.RESUME_AI_CONFIG = {
     // Render backend URL - deployed on Render
-    API_BASE_URL: "https://resumate-backend.xyd8onrendier.com"
+    API_BASE_URL: "https://resumate-backend-xy8b.onrender.com"
 };
